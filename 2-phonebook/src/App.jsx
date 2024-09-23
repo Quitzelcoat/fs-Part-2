35133360cd2bbb67282e01b4a3329aa300f9969b
Haris Saeed <96650020+Quitzelcoat@users.noreply.github.com>
2024-09-23 09:46:00 +0500

    part 2C exe 2.11 complete

--- 2-phonebook/src/App.jsx
+++ 2-phonebook/src/App.jsx
@@ -1,20 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  // const [persons, setPersons] = useState([
+  //   { name: "Arto Hellas", number: "040-123456", id: 1 },
+  //   { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+  //   { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+  //   { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  // ]);
 
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/persons")
+      .then((response) => response.json())
+      .then((data) => setPersons(data));
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -33,9 +40,20 @@ const App = () => {
       number: newNumber,
       id: persons.length + 1,
     };
-    setPersons(persons.concat(personObject));
-    setNewName("");
-    setNewNumber("");
+
+    fetch("http://localhost:3001/persons", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(personObject),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setPersons(persons.concat(data));
+        setNewName("");
+        setNewNumber("");
+      });
   };
 
   const handleNameChange = (event) => {
